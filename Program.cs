d425fed7f8307809d10d814abaab5fa7904872b6
Leo Belanger <leobelanger1996@gmail.com>
2020-08-05 10:20:24 -0700

    initial commit with git ignore

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+
+            string name = "Leo Belanger";
+
+
+
+            // START HERE 
+            Console.WriteLine(name);
+        }
+    }
+}
